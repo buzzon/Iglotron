9dbaf577b59de3977d847cf2c2426da31820bdad
buzzon <buzzondev@gmail.com>
2025-11-18 16:03:54 +0500

    remove build

---
+++ build/CMakeFiles/camera_app.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "" "camera_app_autogen/timestamp" "custom" "camera_app_autogen/deps"
+  "/home/buzzon/Desktop/Iglotron/build/camera_app_autogen/mocs_compilation.cpp" "CMakeFiles/camera_app.dir/camera_app_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/camera_app.dir/camera_app_autogen/mocs_compilation.cpp.o.d"
+  "/home/buzzon/Desktop/Iglotron/camera_app/frangiglwidget.cpp" "CMakeFiles/camera_app.dir/frangiglwidget.cpp.o" "gcc" "CMakeFiles/camera_app.dir/frangiglwidget.cpp.o.d"
+  "/home/buzzon/Desktop/Iglotron/camera_app/main.cpp" "CMakeFiles/camera_app.dir/main.cpp.o" "gcc" "CMakeFiles/camera_app.dir/main.cpp.o.d"
+  "/home/buzzon/Desktop/Iglotron/camera_app/mainwindow.cpp" "CMakeFiles/camera_app.dir/mainwindow.cpp.o" "gcc" "CMakeFiles/camera_app.dir/mainwindow.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
